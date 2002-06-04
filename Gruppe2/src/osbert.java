7505d7e4c0176a16ea770e424c2a5d59c5756b1c
swprakt <msteffen@ifi.uio.no>
2002-06-04 15:48:35 +0000

    CVS TEST

---
+++ Gruppe2/src/osbert.java
@@ -0,0 +1,24 @@
+/*
+ * osbert.java
+ *
+ * Created on 4. Juni 2002, 17:42
+ */
+
+/**
+ *
+ * @author  ben
+ * @version 
+ */
+public class osbert {
+
+    /** Creates new osbert */
+    public osbert() {
+    }
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main (String args[]) {
+    }
+
+}
