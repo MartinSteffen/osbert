e21b52c693b5cb83dedfb1245c5ad6ccde9c2915
swprakt <msteffen@ifi.uio.no>
2002-06-17 21:12:11 +0000

    Jetzt mit Datei IO Ersten Ansätzen einer Listenverwaltung (3.1)

--- Gruppe2/src/osbert.java
+++ Gruppe2/src/osbert.java
@@ -9,14 +9,44 @@
  * @author  ben
  * @version
  */
+import java.util.*;
+import java.io.*;
 public class osbert {
     
     /**
      * @param args the command line arguments
      */
-    
+    //Lade DB
+    // Haupt DB
+    static BilderDB  BDB=new BilderDB();
+    //Temp DB
+    BilderDB TDB=new BilderDB();
     
     public static void main(String args[]) {
+        
+        
+        try {
+            FileInputStream istream = new FileInputStream("bdb.db");
+            ObjectInputStream oin = new ObjectInputStream(istream);
+            BDB=(BilderDB)oin.readObject();
+            oin.close();
+        }
+        catch (ClassNotFoundException CnF){
+            System.out.println("Class Not Found");
+        }
+        catch (FileNotFoundException FnF){
+            System.out.println("File Not Found");
+        }
+        catch (NotSerializableException nse){
+            System.out.println("Not Serializable");
+        }
+        catch (IOException ioe){
+            System.out.println("IO Exception");
+        }
+        
+        
+        
+        
         System.out.println("---------------------");
         System.out.println("|Welcome to Ozzy 0.1|");
         System.out.println("---------------------");
@@ -27,6 +57,11 @@ public class osbert {
         tokenReader.lowerCaseMode(true);
         int checkToken=0;
         double inputToken=0;
+        
+        
+        
+        
+        
         menu:
             while ( true ) {
                 System.out.println("");
@@ -65,7 +100,20 @@ public class osbert {
                             menu3();
                             break;
                         case 4:
-                            
+                            try {
+                                FileOutputStream ostream = new FileOutputStream("bdb.db");
+                                ObjectOutputStream oout = new ObjectOutputStream(ostream);
+                                oout.writeObject(BDB);
+                                oout.flush();
+                                oout.close();
+                            }
+                            catch (FileNotFoundException FnF){
+                                System.out.println("File Not Found");
+                            }
+                            catch (NotSerializableException nse){
+                                System.out.println("Not Serializable");
+                            }
+                            catch (IOException ioe){}
                             break menu;
                     }
                 }
@@ -217,7 +265,8 @@ public class osbert {
                     switch ((int)inputToken) {
                         
                         case 1:
-                                
+                            editbdb();
+                            
                             break;
                         case 2:
                             
@@ -230,5 +279,65 @@ public class osbert {
                 
             }
     }
-    
+    public static void editbdb(){
+        java.io.InputStreamReader isr= new java.io.InputStreamReader(System.in);
+        java.io.StreamTokenizer tokenReader= new java.io.StreamTokenizer(isr);
+        tokenReader.lowerCaseMode(true);
+        int checkToken=0;
+        int intToken=0;
+        int i=0;
+        
+        String stringToken=null;
+        double inputToken=0;
+        //Datenbank vornereiten
+        //Iterator dbIter=BDB.Iterator();
+        Iterator dbIter=BDB.iterator();
+        editdb:
+            while (true){
+                System.out.println();
+                System.out.println();
+                System.out.println("Edieren der Bilderdatenbank");
+                System.out.println();
+                System.out.println("Geben Sie die Datensatznummer ein, die Sie bearbeiten wollen.");
+                System.out.println("Geben Sie l ein um die Liste der Datensätze anzeigen zu lassen.");
+                System.out.println("Geben Sie x ein um in der Menühierachie eine Stufe nach oben zu gelangen.");
+                
+                try{
+                    checkToken=tokenReader.nextToken();
+                }
+                catch (java.io.IOException ioe){};
+                
+                if (tokenReader.TT_WORD==checkToken){
+                    stringToken=tokenReader.sval;
+                    if (stringToken.equals("x"))break editdb;
+                    else {
+                        if (stringToken.equals("l")){
+                            while (dbIter.hasNext()){
+                                System.out.print(i++);
+                                System.out.print("   ");
+                                
+                                ((Bild)(dbIter.next())).output();
+                            }
+                            i=0;
+                            dbIter=BDB.iterator();
+                                
+                        }
+                        else {
+                            break editdb;
+                        }
+                    }
+                    
+                    
+                }
+                if (tokenReader.TT_NUMBER==checkToken){
+                    intToken=(int)tokenReader.nval;
+                    
+                    
+                    
+                    
+                    
+                }
+            }
+            
+    }
 }
